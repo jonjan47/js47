d3c57bc9f48b035bae772cdbb4b95aaa67a822d9
jonjan47 <148625742+jonjan47@users.noreply.github.com>
2023-12-06 22:20:26 +0500

    Add files via upload

---
+++ ja.js
@@ -0,0 +1,7 @@
+//window  object :is not creat js its creat window  it creat automatically creat browser
+// it is a global object with lot of properties & method
+//--> DOM :when a web page is loaded the browser creates a Document Object Model(DOM) of the page
+//console.log(document);
+//console.dir(document);
+//-->DOM Manipulation
+//1>slecting with Id
